0b49a543ca8b99b9d4ddb70c9198307128277f97
shahariyar Nafis <72545687+shahariyar77@users.noreply.github.com>
2020-11-30 19:24:55 +0600

    Add files via upload

---
+++ 100-carousel-team/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.team-carousel').owlCarousel({
+        item:3,
+        loop:true,
+        margin:30,
+        nav:true,
+        navText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+        dots:false,
+        autoplay:true
+    });
+});
+    
